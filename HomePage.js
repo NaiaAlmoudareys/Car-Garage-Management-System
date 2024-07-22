89166d7d4d459e7a06dc96c847721454901a7e30
NaiaAlmoudareys <151873469+NaiaAlmoudareys@users.noreply.github.com>
2024-07-21 23:22:31 -0400

    Create HomePage.js

---
+++ HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to Car Garage Management System</h1>
+            <p>Manage your car services efficiently and effectively.</p>
+        </div>
+    );
+}
+
+export default HomePage;
